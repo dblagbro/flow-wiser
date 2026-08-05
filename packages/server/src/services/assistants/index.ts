8d3563fc0b34637f7b232892f1abad4bd27a8977
Ilango Rajagopal <ilango.rajagopal@flowiseai.com>
2026-08-05 14:20:58 +0530

    fix: flowise-603

--- packages/server/src/services/assistants/index.ts
+++ packages/server/src/services/assistants/index.ts
@@ -184,7 +184,8 @@ const deleteAssistant = async (assistantId: string, isDeleteBoth: any, workspace
         try {
             const assistantDetails = JSON.parse(assistant.details)
             const credential = await appServer.AppDataSource.getRepository(Credential).findOneBy({
-                id: assistant.credential
+                id: assistant.credential,
+                workspaceId: workspaceId
             })
 
             if (!credential) {
